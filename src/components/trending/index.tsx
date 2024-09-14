142cb6603b232b1b0eb235010d7614bcf4e9589b
LeVuWan <levuwan@gmail.com>
2024-09-14 12:59:40 +0700

    v3

--- src/components/trending/index.tsx
+++ src/components/trending/index.tsx
@@ -5,7 +5,7 @@ const Trending = () => {
     return (
         <>
             <h3 className="card">Trending</h3>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-10 mb-12">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 lg:grid-cols-4 gap-10 mb-12">
                 {moviesData.map((movie, index) => (
                     <MovieCard key={index} movie={movie} />
                 ))}
